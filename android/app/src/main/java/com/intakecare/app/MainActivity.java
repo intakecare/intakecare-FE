dd60b0ffa8fb704ff6b81f905a8a4e3f196ad2c8
Giulia Ferri <giulia3.ferri@mail.polimi.it>
2024-03-22 18:57:09 +0100

    Capacitorjs added, baseFetch updated to handle phone requests

---
+++ android/app/src/main/java/com/intakecare/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.intakecare.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/api/baseFetch.ts
+++ src/api/baseFetch.ts
@@ -1,4 +1,13 @@
 import axios, { Method } from "axios";
+import { Capacitor } from '@capacitor/core';
+const BASE_URL = 'https://intake-care-de7b20968242.herokuapp.com';
+export default async (url: string, method: string, options = {}) => {
+    const apiUrl = getApiUrl(url);
+    return axios({ method: method as Method, url: apiUrl, ...options });
+};
 
-export default async (url: string, method: string, options = {}) =>
-  axios({ method: method as Method, url, ...options });
+const getApiUrl = (url: string) => {
+    // Determine the final API URL based on whether the application is running on a native platform or not
+    return Capacitor.isNativePlatform() ? BASE_URL + url.substring(4) : url;
+};
+
